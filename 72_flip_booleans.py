cf6b77c96ece79d652c451cdfc931479a536acad
eelanpy <76450488+eelanpy@users.noreply.github.com>
2020-12-21 14:18:06 -0500

    Added flip booleans file

---
+++ 72_flip_booleans.py
@@ -0,0 +1,13 @@
+# 72_flip_booleans:
+
+def flip(arg):
+    if arg == '0':
+        return "boolean expected"
+    elif arg == True:
+        return False
+    elif arg == False:
+        return True
+    else:
+        return "boolean expected"
+
+print(flip(234))
